ba1e947b16555ab0d56b499252a28dddc2113b72
AstreaRider <rizkiramadhan34@gmail.com>
2024-08-24 06:33:01 +0700

    finalize fiber & database connection setup

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/AstreaRider/notes-api-demo/database"
+	"github.com/gofiber/fiber/v2"
+)
 
 
 func main()  {
 	app := fiber.New()
+
+	database.ConnectDB()
 
 	app.Get("/", func(c *fiber.Ctx) error {
 		err := c.SendString("And the API is UP!")
